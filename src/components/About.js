aaab1eadf94dc0d78a86fed58448288eaf1089a2
Nicholas Kibet <nickkorir08@gmail.com>
2024-08-11 10:15:25 +0300

    all components added

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Nick is a web developer with a passion for creating dynamic web applications.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
